6495f4e14a9b77ef422d95179d52a146b75de544
Ayomide <oluwashinabajo@gmail.com>
2022-03-04 19:06:25 +0100

    add logic for outputting blank spaces

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use clap::{Arg, Command};
+// use clap::{Arg, Command};
 use rand::seq::SliceRandom;
 use std::io;
 
@@ -8,15 +8,10 @@ fn main() {
         no_of_guesses: i8,
         available_alphabets: Vec<char>,
         list_of_words_to_guess_from: Vec<String>,
-        score: i8,
+        ouput_string: Vec<char>,
         max_tries: i8,
         guess: String,
         correct_guesses: Vec<char>,
-    }
-
-    struct PlayerGuesser {
-        guess: char,
-        tries: i8,
     }
 
     impl PlayerRoot {
@@ -25,7 +20,7 @@ fn main() {
             no_of_guesses: i8,
             available_alphabets: Vec<char>,
             list_of_words_to_guess_from: Vec<String>,
-            _score: i8,
+            ouput_string: Vec<char>,
             max: i8,
             guess: String,
             correct_guesses: Vec<char>,
@@ -35,7 +30,7 @@ fn main() {
                 no_of_guesses,
                 available_alphabets,
                 list_of_words_to_guess_from,
-                score: 0,
+                ouput_string,
                 max_tries: max,
                 guess,
                 correct_guesses,
@@ -48,7 +43,7 @@ fn main() {
             word.to_string()
         }
         //Still don't know if I should keep this function, it's basically for producing alpahbets
-        fn produce_alphabets(self) {
+        fn _produce_alphabets(self) {
             let letters = String::from(
                 "A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y,Z",
             );
@@ -71,19 +66,26 @@ fn main() {
         'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
         'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
     ];
+    let guess_vec: Vec<char> = random_word.clone().chars().collect();
+    // println!("guess vec{:?}, guess capacity{:?}", guess_vec, guess_vec.capacity());
     let guess_chars = vec![];
+    let ouput_string_vec = vec!['_'; guess_vec.len()];
+   println!("Welcome to the hangman game built with rust!, please enter a letter");
+   println!("You already know what this is :D");
+    println!("Fill in the blank spaces{:?}", ouput_string_vec);
     let mut player_one = PlayerRoot::new(
         &random_word,
         0,
         letters,
         list_of_words,
-        0,
+        ouput_string_vec,
         6,
         "".to_string(),
         guess_chars,
     );
+
     loop {
-      println!("Welcome to the hangman game built with rust!, please enter a letter");
+     
 
         //Takes in an input
         //todo unwrap the input or validate the input
@@ -93,6 +95,7 @@ fn main() {
             .expect("Failed to read line");
         let altered_guess: char = guess.trim().chars().next().unwrap();
         player_one.no_of_guesses += 1;
+
         'outer: for n in random_word.char_indices() {
             if !player_one.correct_guesses.contains(&n.1) {
                 if n.1 == altered_guess {
@@ -106,8 +109,10 @@ fn main() {
                 player_one.correct_guesses, player_one.no_of_guesses
             )
         }
+
+        // if ()
         if player_one.max_tries == player_one.no_of_guesses {
-          println!("GAME OVER!!!");
+            println!("GAME OVER!!!");
             break;
         }
     }
