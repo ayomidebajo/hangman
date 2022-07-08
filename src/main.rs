8748126e755e1dd128ae9d6663f4f3be8d9153f9
Ayomide Bajo <oluwashinabajo@gmail.com>
2022-07-08 13:36:24 +0100

    change names, clean up code

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use rand::seq::SliceRandom;
 use std::io;
 #[allow(dead_code)]
+
 fn main() {
-    struct GameAssets {
+    struct PlayerRoot {
         word: String,
         no_of_guesses: i8,
         available_alphabets: Vec<char>,
@@ -13,7 +14,12 @@ fn main() {
         correct_guesses: Vec<char>,
     }
 
-    impl GameAssets {
+    struct PlayerGuesser {
+        guess: char,
+        tries: i8,
+    }
+
+    impl PlayerRoot {
         fn new(
             word: &str,
             no_of_guesses: i8,
@@ -23,8 +29,8 @@ fn main() {
             max: i8,
             guess: String,
             correct_guesses: Vec<char>,
-        ) -> GameAssets {
-            GameAssets {
+        ) -> PlayerRoot {
+            PlayerRoot {
                 word: String::from(word),
                 no_of_guesses,
                 available_alphabets,
@@ -41,16 +47,8 @@ fn main() {
             // println!("word {:?}", word);
             word.to_string()
         }
-        //Still don't know if I should keep this function, it's basically for producing alpahbets
-        // fn _produce_alphabets(self) {
-        //     let letters = String::from(
-        //         "A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y,Z",
-        //     );
-        //     for item in letters.split(",") {
-        //         println!("{:?}", item)
-        //     }
-        // }
     }
+
     //list of words for the game
     let list_of_words = vec![
         "hunting".to_string(),
@@ -60,29 +58,39 @@ fn main() {
         "something".to_string(),
         "notified".to_string(),
     ];
-    let random_word = GameAssets::generate_random_word(&list_of_words);
+
+    let random_word = PlayerRoot::generate_random_word(&list_of_words);
+
     let letters = vec![
         'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
         'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
     ];
-    let guess_vec: Vec<char> = random_word.clone().chars().collect();
+
     let guess_chars = vec![];
-    let ouput_string_vec = vec!['_'; guess_vec.len()];
-    println!("Welcome to the hangman game built with rust!, please enter a letter");
-    println!("You already know what this is :D");
-    println!("Fill in the blank spaces{:?}", ouput_string_vec);
-    let mut player_one = GameAssets::new(
+
+    // for our simple UI
+    let guess_vec: Vec<char> = random_word.clone().chars().collect();
+    let output_string_vec = vec!['_'; guess_vec.len()];
+
+    let mut player_one = PlayerRoot::new(
         &random_word,
         0,
         letters,
         list_of_words,
-        ouput_string_vec,
+        output_string_vec,
         10,
         "".to_string(),
         guess_chars,
     );
 
+    println!("Welcome to the hangman game built with rust!, please enter a letter");
+    println!(
+        "Fill in the blank spaces{:?}, no of guesses made {:?}, no of max tries {:?}",
+        player_one.output_string, player_one.no_of_guesses, player_one.max_tries
+    );
+
     loop {
+
         //Takes in an input
         //Todo Check if input is more than one char
         let mut guess = String::from("");
@@ -95,22 +103,23 @@ fn main() {
                 println!("No letter inputted,type a letter!");
                 //It complains it needs a char :D
                 '0'
-                // break;
             }
         };
 
+
+// Checks if guess is valid
         if !player_one.output_string.contains(&altered_guess) {
             player_one.no_of_guesses += 1;
 
             if !player_one.word.contains(altered_guess) {
                 let guess_score = player_one.max_tries - player_one.no_of_guesses;
                 println!(
-                        "Wrong guess\nFill in the blank spaces{:?} no of guesses remaining {:?}",
-                        player_one.output_string, guess_score
-                    );
+                    "Wrong guess\nFill in the blank spaces{:?} no of guesses remaining {:?}",
+                    player_one.output_string, guess_score
+                );
             }
 
-
+            // loops through the word, check if guess is correct, reduces number of guess by one
             for n in player_one.word.char_indices() {
                 if n.1 == altered_guess {
                     let guess_score = player_one.max_tries - player_one.no_of_guesses;
@@ -122,16 +131,18 @@ fn main() {
                         "Fill in the blank spaces{:?} no of guesses remaining {:?}",
                         player_one.output_string, guess_score
                     );
-                } 
+                }
             }
         } else {
             println!("That letter is taken!!! guess again")
         }
+        // If the player wins
         if player_one.correct_guesses.len() == guess_vec.len() {
             println!("YOU WIN!!");
             break;
         }
 
+        // If the player loses
         if player_one.max_tries == player_one.no_of_guesses {
             println!("GAME OVER!!! \n THE WORD IS {:?}", player_one.word);
             break;
